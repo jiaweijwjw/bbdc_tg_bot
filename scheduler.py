82cffb8c1c87f7d8b3d9a471d3ae989c7b1c7e8f
jiawei <jiaweilim97@gmail.com>
2021-06-07 14:12:58 +0800

    test

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job("interval", minutes=15, jitter=120)
+@scheduler.scheduled_job("interval", minutes=1, jitter=120)
 def timed_job():
     exec(open("scraper.py").read())
 
